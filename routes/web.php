99ae29cfccbfc5463dec9ce7b2dd8e76b1d385ae
tienduy1912 <hoangmacphong1912@gmail.com>
2025-06-14 20:09:17 +0700

    sửa lại routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RoleController;
 use App\Http\Controllers\Admin\DashBoardController;
-use App\Http\Controllers\Admin\OriginController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\OriginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,11 +23,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('admin')->name('admin.')->group(function () {
-    // Trang dashboard
-    Route::get('/', [DashBoardController::class, 'index'])->name('dashboard');
-   
-     // Categories
+Route::prefix('admin')->name('admin.')->middleware('admin')->group(function () {
+    // Dashboard
+    Route::get('/dashboard', [DashBoardController::class, 'index'])->name('dashboard');
+
+    // Categories
+    Route::put('categories/{id}/toggle-status', [CategoryController::class, 'toggleStatus'])->name('categories.toggleStatus');
     Route::resource('categories', CategoryController::class)->names([
         'index'   => 'categories.index',
         'create'  => 'categories.create',
@@ -33,12 +37,9 @@ Route::prefix('admin')->name('admin.')->group(function () {
         'update'  => 'categories.update',
         'destroy' => 'categories.destroy',
     ]);
-   Route::put('categories/{id}/toggle-status', [CategoryController::class, 'toggleStatus'])->name('categories.toggleStatus');
- 
 
- // ORIGINS
-    
-    Route::resource('Origins', OriginController::class)->names([
+    // Origins 
+    Route::resource('origins', OriginController::class)->names([
         'index'   => 'origins.index',
         'create'  => 'origins.create',
         'store'   => 'origins.store',
@@ -47,5 +48,27 @@ Route::prefix('admin')->name('admin.')->group(function () {
         'destroy' => 'origins.destroy',
     ]);
 
-    
+    // Roles
+    Route::resource('roles', RoleController::class);
 });
+// User dashboard
+Route::get('/user/dashboard', function () {
+    return view('user.dashboard');
+})->name('user.dashboard')->middleware('auth');
+
+
+// Register
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+
+// Route đăng nhập
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::get('/dashboard', function () {
+    return 'Bạn đã đăng nhập!';
+})->middleware('auth');
+
+
+// Route đăng xuất
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
