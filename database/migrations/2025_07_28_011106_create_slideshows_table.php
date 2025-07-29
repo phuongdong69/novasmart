1528cb2458732e1f39b7512b4ecf3eb293468f77
Nguyễn Đỗ Long <105410983+yepiamlong@users.noreply.github.com>
2025-07-30 06:06:31 +0700

    Update 2025_07_28_011106_create_slideshows_table.php

--- database/migrations/2025_07_28_011106_create_slideshows_table.php
+++ database/migrations/2025_07_28_011106_create_slideshows_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('slideshows', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('link')->nullable();
+            $table->integer('order')->default(0);
+            $table->boolean('is_active')->default(true);
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
